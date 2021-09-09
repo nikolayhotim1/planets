eeb23405d897872711314264cf9878de0304e0e3
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-09 23:26:46 +0300

    refactoring: change color of text in greenplanet paragraph

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function init() {
     let planet = document.getElementById('greenplanet');
     planet.innerHTML = 'Red Alert: hit by phaser fire!';
+    planet.setAttribute('class', 'redtext');
 }
 
 window.onload = init;
